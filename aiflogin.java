d680b3385240ea0a9e9027b7508df127363033a2
lohit3251 <lohit6897@gmail.com>
2025-08-25 12:52:11 +0530

    Update aiflogin.java with [brief description of changes]

--- aiflogin.java
+++ aiflogin.java
@@ -8,12 +8,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
 
 public class aiflogin {
 
-	public static void main(String[] args) throws InterruptedException {
+	//public static void main(String[] args) throws InterruptedException {
 		
-		  
+		  @Test void login()
+		  {
 	
 	ChromeOptions options = new ChromeOptions();
 	WebDriver driver = new ChromeDriver(options);
@@ -39,22 +41,7 @@ public class aiflogin {
 	driver.findElement(By.id("exampleInputDomain1")).sendKeys(domain);
 	driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
 }
+
